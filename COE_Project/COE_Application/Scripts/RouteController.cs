8c848e8af4c43af693a8c4435898049b5f69f51c
jfeze1 <jfeze1@studentmail.nait.ca>
2019-11-20 14:27:25 -0700

    RouteController update

---
+++ COE_Project/COE_Application/Scripts/RouteController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additionnal Namesapces
+using System.ComponentModel;
+using COESystem.Data.DTOs;
+using COESystem.Data.Entities;
+using COESystem.Data.POCOs;
+using COESystem.DAL;
+#endregion
+
+namespace COESystem.BLL
+{
+    [DataObject]
+    public class RouteController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<RouteStatus> RouteStatus_List(int season, int yardId)
+        {
+            using (var context = new COESystemContext())
+            {
+                var RouteList = from site in context.Sites
+                                orderby site.Community.Name ascending
+                                where site.Season.SeasonYear == 2018 && site.Yard.YardID == 1
+                                select new RouteStatus
+                                {
+                                    Pin = site.Pin,
+                                    Community = site.Community.Name,
+                                    Address = site.StreetAddress,
+                                    Description = site.Neighbourhood,
+                                    Area = site.Area,
+                                    Notes = site.Notes,
+                                    JobDone = (from crewSite in context.CrewSites
+                                               where crewSite.SiteID == site.SiteID
+                                               select new SiteStatus
+                                               {
+                                                   SBM = crewSite.SBM == true ? crewSite.Crew.TodayDate : (DateTime?)null,
+                                                   Mulch = crewSite.Mulch == true ? crewSite.Crew.TodayDate : (DateTime?)null,
+                                                   Prune = crewSite.Prune == true ? crewSite.Crew.TodayDate : (DateTime?)null,
+                                               }).ToList()
+                                };
+                return RouteList.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<RouteStatus> RouteStatus_List(int season, int yardId, string type)
+        {
+            using (var context = new COESystemContext())
+            {
+                var RouteList = from site in context.Sites
+                                orderby site.Community.Name ascending
+                                where site.Season.SeasonYear == 2018 && site.Yard.YardID == 1 && site.SiteType.SiteTypeDescription == "B"
+                                select new RouteStatus
+                                {
+                                    Pin = site.Pin,
+                                    Community = site.Community.Name,
+                                    Address = site.StreetAddress,
+                                    Description = site.Neighbourhood,
+                                    Area = site.Area,
+                                    Notes = site.Notes,
+                                    JobDone = (from crewSite in context.CrewSites
+                                               where crewSite.SiteID == site.SiteID
+                                               select new SiteStatus
+                                               {
+                                                   SBM = crewSite.SBM == true ? crewSite.Crew.TodayDate : (DateTime?)null,
+                                                   Mulch = crewSite.Mulch == true ? crewSite.Crew.TodayDate : (DateTime?)null,
+                                                   Prune = crewSite.Prune == true ? crewSite.Crew.TodayDate : (DateTime?)null,
+                                               }).ToList()
+                                };
+                return RouteList.ToList();
+            }
+        }
+    }
+}
